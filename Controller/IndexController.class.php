078ca405205bf25473f5ddf0cddfb4ca8dec6edf
Jayin Ton <tonjayin@gmail.com>
2017-01-14 20:45:14 +0800

    Initial commit

---
+++ Controller/IndexController.class.php
@@ -0,0 +1,16 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | Copyright (c) Zhutibang.Inc 2016 http://zhutibang.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: zhlhuang <zhlhuang888@foxmail.com>
+// +----------------------------------------------------------------------
+
+namespace Wechat\Controller;
+use Wechat\Service\OpenService;
+
+class IndexController extends WxBaseController{
+    public function index(){
+        echo '';
+    }
+}
