ac50b20cbb28ea8b5126dbb9db5597a437e463b5
Jayin Ton <273942569@qq.com>
2017-02-05 14:14:10 +0800

    Update Config.inc.php

--- Config.inc.php
+++ Config.inc.php
@@ -12,7 +12,7 @@ return array(
 	//模块简介
 	'introduce' => '微信授权登录，api调用',
 	//模块介绍地址
-	'address' => 'http://ztbcms.com',
+	'address' => 'http://doc.ztbcms.com/module/wechat/',
 	//模块作者
 	'author' => 'ZtbCMS-zhlhuang',
 	//作者地址
