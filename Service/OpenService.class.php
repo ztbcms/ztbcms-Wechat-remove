f11b3d9e85d2d069d36d5d485a5d60724b49c475
zhlhuang <364626853@qq.com>
2017-04-12 11:58:34 +0800

    openService 适应v3版本修改

--- Service/OpenService.class.php
+++ Service/OpenService.class.php
@@ -1,19 +1,44 @@
 <?php
 
 // +----------------------------------------------------------------------
-// | Copyright (c) Zhutibang.Inc 2016 http://zhutibang.cn All rights reserved.
+// | Copyright (c) Zhutibang.Inc 2017 http://zhutibang.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Author: zhlhuang <zhlhuang888@foxmail.com>
 // | ztbOpen操作
 // +----------------------------------------------------------------------
+
 namespace Wechat\Service;
 
 class OpenService {
     private $domain = 'http://open.ztbopen.cn';
-    private $config = null;
+    private $open_app = null;
 
-    function __construct() {
-        $this->config = cache('Config');
+    function __construct($open_app_id = null) {
+        if (!$open_app_id) {
+            //如果传null 这是默认的app
+            $this->open_app = M('WechatApp')->order('`default` DESC')->find();
+        } else {
+            $this->open_app = M('WechatApp')->where(['open_app_id' => $open_app_id])->find();
+        }
+    }
+
+
+    public function getWxpayConfig($openid, $out_trade_no, $fee, $notify_url = '', $body = '', $detail = '') {
+        $time = time(); //当前时间戳
+        $sign = $this->getSign($time);
+        $api_url = $this->domain . "/api/wxpay_api/get_prepay_info/app_id/" . $this->open_app['open_app_id'] . ".html?time={$time}&sign={$sign}";
+        $send_data = array(
+            'openid' => $openid,
+            'out_trade_no' => $out_trade_no,
+            'fee' => $fee,
+            'notify_url' => $notify_url,
+            'body' => $body,
+            'detail' => $detail,
+        );
+        $res_json = $this->post($api_url, $send_data);
+        $res = json_decode($res_json, 1);
+
+        return $res ? $res : $res_json;
     }
 
     /**
@@ -22,17 +47,18 @@ class OpenService {
      * @param $img_id string 图片id face++唯一图片id
      * @return mixed
      */
-    public function get_face_detect_info($img_id) {
+    public function getFaceDetectInfo($img_id) {
         $time = time(); //当前时间戳
-        //注意配置的模块，如果配置在Wechat/Conf中，其他模块调用可能出问题
-        $sign = $this->sign_encode($this->config['open_app_id'] , $time , $this->config['open_secret_key']);
-        $api_url = $this->domain . "/api/face_api/get_detect_info/app_id/" . $this->config['open_app_id'] . ".html?time={$time}&sign={$sign}";
+        $sign = $this->getSign($time);
+        $api_url = $this->domain . "/api/face_api/get_detect_info/app_id/" . $this->open_app['open_app_id'] . ".html?time={$time}&sign={$sign}";
         $send_data = array(
             'img_id' => $img_id,
         );
-        $res = $this->post($api_url, $send_data);
 
-        return json_decode($res, 1);
+        $res_json = $this->post($api_url, $send_data);
+        $res = json_decode($res_json, 1);
+
+        return $res ? $res : $res_json;
     }
 
     /**
@@ -41,17 +67,18 @@ class OpenService {
      * @param  $img_url string 需要识别的图片url
      * @return mixed
      */
-    public function get_face_detect($img_url) {
+    public function getFaceDetect($img_url) {
         $time = time(); //当前时间戳
-        //注意配置的模块，如果配置在Wechat/Conf中，其他模块调用可能出问题
-        $sign = $this->sign_encode($this->config['open_app_id'] , $time , $this->config['open_secret_key']);
-        $api_url = $this->domain . "/api/face_api/detect/app_id/" . $this->config['open_app_id'] . ".html?time={$time}&sign={$sign}";
+        $sign = $this->getSign($time);
+        $api_url = $this->domain . "/api/face_api/detect/app_id/" . $this->open_app['open_app_id'] . ".html?time={$time}&sign={$sign}";
         $send_data = array(
             'img_url' => $img_url,
         );
-        $res = $this->post($api_url, $send_data);
 
-        return json_decode($res, 1);
+        $res_json = $this->post($api_url, $send_data);
+        $res = json_decode($res_json, 1);
+
+        return $res ? $res : $res_json;
     }
 
     /**
@@ -67,7 +94,7 @@ class OpenService {
     public function send_template($openid, $template_id, $data, $url = null, $topcolor = '#f7f7f7') {
         $time = time();//当前时间戳
         //注意配置的模块，如果配置在Wechat/Conf中，其他模块调用可能出问题
-        $sign = $this->sign_encode($this->config['open_app_id'] , $time , $this->config['open_secret_key']);
+        $sign = $this->sign_encode($this->config['open_app_id'], $time, $this->config['open_secret_key']);
         $api_url = $this->domain . "/api/template_api/send_template/app_id/" . $this->config['open_app_id'] . ".html?time={$time}&sign={$sign}";
         $send_data = array(
             'touser' => $openid,
@@ -76,28 +103,21 @@ class OpenService {
             'topcolor' => $topcolor,
             'data' => json_encode($data)
         );
-        $res = $this->post($api_url, $send_data);
+        $res_json = $this->post($api_url, $send_data);
+        $res = json_decode($res_json, 1);
 
-        return json_decode($res, 1);
+        return $res ? $res : $res_json;
     }
 
-    /**
-     * api调用签名
-     *
-     * @param string $appid       open平台的appid
-     * @param int $time        当前时间戳
-     * @param string $secret_key  open平台的私钥
-     * @return string
-     */
-    public function sign_encode($appid, $time, $secret_key) {
-        return md5($appid . $time . $secret_key);
+    public function getSign($time) {
+        return md5($this->open_app['open_app_id'] . $time . $this->open_app['open_secret_key']);
     }
 
     /**
      * get method
      *
      * @param string $url
-     * @param array $param
+     * @param array  $param
      * @return mixed
      */
     function get($url, $param = array()) {
