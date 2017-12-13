96ef32efaab9ca79857bbf9d00af2b2cbfe4f382
Jayin Ton <tonjayin@gmail.com>
2017-12-13 09:53:24 +0800

    v3.0.3.2

--- Config.inc.php
+++ Config.inc.php
@@ -20,7 +20,7 @@ return array(
 	//作者邮箱
 	'authoremail' => 'zhlhuang888@foxmail.com',
 	//版本号，请不要带除数字外的其他字符
-	'version' => '3.0.3.1',
+	'version' => '3.0.3.2',
 	//适配最低ZtbFCMS版本，
 	'adaptation' => '3.0.0.0',
 	//签名
